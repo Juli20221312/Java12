2bf8606abffd662e87dd3945938d7082a3626477
Julia <vondel@mail.ru>
2023-02-28 13:40:18 +0600

    Add tests with limit 5

--- src/test/java/MoviesManagerTest.java
+++ src/test/java/MoviesManagerTest.java
@@ -147,5 +147,41 @@ public class MoviesManagerTest {
         Assertions.assertArrayEquals(expected, actual);
     }
 
+    @Test
+    public void addFilms5() {
+        MoviesManager manager = new MoviesManager(5);
+        manager.add("Film I");
+        manager.add("Film II");
+        manager.add("Film III");
+        manager.add("Film IV");
+        manager.add("Film V");
+        manager.add("Film VI");
+
+        String[] expected = {"Film I", "Film II", "Film III", "Film IV", "Film V", "Film VI"};
+        String[] actual = manager.findAll();
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void findLastEleven5() {
+        MoviesManager manager = new MoviesManager(5);
+
+        manager.add("Film I");
+        manager.add("Film II");
+        manager.add("Film III");
+        manager.add("Film IV");
+        manager.add("Film V");
+        manager.add("Film VI");
+        manager.add("Film VII");
+        manager.add("Film VIII");
+        manager.add("Film IX");
+        manager.add("Film X");
+        manager.add("Film XI");
+
+        String[] expected = { "Film XI", "Film X", "Film IX", "Film VIII", "Film VII"};
+        String[] actual = manager.findLast();
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
 
 }
